820d0f3b0eb1381e5bedc8358aa77db1fbc698d3
TotalElderBerry <lisondrabrian48@gmail.com>
2023-11-26 13:39:50 +0800

    change constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,28 +1,34 @@
-const categorythumbs = 'assets/category-thumbs/'
-export const categories = [
-	{
-		name: "Women's Apparel",
-		image: categorythumbs + 'women.png'
-	},
-	{
-		name: "Men's Apparel",
-		image: categorythumbs + 'men.png'
-	},
-	{
-		name: "Children's Apparel",
-		image: categorythumbs + 'children.png'
-	},
-	{
-		name: "Mobile & Gadgets",
-		image: categorythumbs + 'gadget.png'
-	},
-	{
-		name: "Computers & Accessories",
-		image: categorythumbs + 'computer.png'
-	},
-	{
-		name: "Books",
-		image: categorythumbs + 'books.png'
-	}
-]
+import women from 'assets/category-thumbs/women.png';
+import men from 'assets/category-thumbs/men.png';
+import children from 'assets/category-thumbs/children.png';
+import gadget from 'assets/category-thumbs/gadget.png';
+import computer from 'assets/category-thumbs/computer.png';
+import books from 'assets/category-thumbs/books.png';
 
+const categorythumbs = 'assets/category-thumbs/';
+export const categories = [
+  {
+    name: "Women's Apparel",
+    image: women
+  },
+  {
+    name: "Men's Apparel",
+    image: men
+  },
+  {
+    name: "Children's Apparel",
+    image: children
+  },
+  {
+    name: "Mobile & Gadgets",
+    image: gadget
+  },
+  {
+    name: "Computers & Accessories",
+    image: computer
+  },
+  {
+    name: "Books",
+    image: books
+  }
+];
